ab8aaabaedff064b0e4f4ddadeb7a1aab79ed9d0
Eric Schles <ericschles@gmail.com>
2016-11-09 11:17:34 -0500

    added help and expanded some variables names

--- data_capture/management/commands/finddupes.py
+++ data_capture/management/commands/finddupes.py
@@ -5,6 +5,30 @@ from contracts.models import Contract
 
 @click.command()
 def command():
+    help='''\
+    Finds duplicates across sin, contractor_site, education level, min years of experience, second year price,
+    hourly rate year 2, hourly rate year 5, next year price, hourly rate year 3, hourly rate year 4, contract year.
+    
+    This code assumes, vendor name, labor category, individual piid and current price are the same.
+ 
+    example run (from root directory of calc):
+
+    python manage.py finddupes
+
+    result:
+
+    sin                       2820
+    contractor_site           844
+    education_level           206
+    min_years_experience      142
+    second_year_price         8
+    hourly_rate_year2         8
+    hourly_rate_year5         8
+    next_year_price           8
+    hourly_rate_year3         6
+    hourly_rate_year4         4
+    contract_year             4
+    '''
     identical_fields = [
         'vendor_name',
         'labor_category',
@@ -25,19 +49,20 @@ def command():
             ])
         }
     )
-    cmap = {}
+
+    contract_map = {}
     for c in contracts:
-        cmap[c.id] = c
+        contract_map[c.id] = c
     already_visited = {}
     counts = {}
     fields = [
         field for field in Contract._meta.get_fields()
         if not (field.name == 'id' or field.is_relation)
     ]
-    for c1 in cmap.values():
+    for c1 in contract_map.values():
         if c1.id in already_visited:
             continue
-        c2 = cmap[c1.duplicate_id]
+        c2 = contract_map[c1.duplicate_id]
         already_visited[c2.id] = True
         different_fields = tuple([
             field.name for field in fields
