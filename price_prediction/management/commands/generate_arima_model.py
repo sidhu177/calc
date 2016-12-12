7f51967a68af20e3fa802e550648be9ed3918990
Eric Schles <ericschles@gmail.com>
2016-12-12 14:38:11 -0500

    removing basinhopping - doesn't appear to play nice with ARIMA

--- price_prediction/management/commands/generate_arima_model.py
+++ price_prediction/management/commands/generate_arima_model.py
@@ -8,7 +8,7 @@ import statsmodels.api as sm
 import statistics
 from functools import partial
 import code
-from scipy.optimize import brute
+from scipy.optimize import brute, basinhopping, differential_evolution
 from django.core.management import BaseCommand
 from optparse import make_option
 
@@ -17,7 +17,7 @@ from optparse import make_option
 def objective_function(data, order):
     return sm.tsa.ARIMA(data, order).fit().aic
 
-def grid_search(data,optimizer,**kwargs):
+def brute_search(data):
     print("got here with no errors")
     obj_func = partial(objective_function, data)
     # Back in graduate school professor Lecun said in class that ARIMA models 
@@ -32,7 +32,7 @@ def grid_search(data,optimizer,**kwargs):
             if upper_bound_AR < 0 or upper_bound_I < 0 or upper_bound_MA < 0:
                 grid_not_found = False
             grid = (slice(1, upper_bound_AR, 1),slice(1, upper_bound_I, 1),slice(1, upper_bound_MA, 1))
-            order = optimizer(obj_func, grid, **kwargs)
+            order = brute(obj_func, grid, finish=None)
             return order, obj_func(order)
         except Exception as e: #found here: http://stackoverflow.com/questions/4308182/getting-the-exception-value-in-python
             error_string = str(e)
@@ -48,6 +48,7 @@ def grid_search(data,optimizer,**kwargs):
         grid = ( slice(1, 2, 1), slice(1, 2, 1), slice(1, 2, 1))
         order = brute(obj_func, grid, finish=None)
         return order, obj_func(order)
+        
     except: #however we don't always meet invertibility conditions
         #Here we explicitly test for a single MA or AR process being a better fit
         #If either has a lower (better) aic score we return that model order
@@ -63,9 +64,9 @@ def model_search(data):
     Optimizers supported:
     * brute
     """
+    print("got to start of model search")
     results = []
-    results.append(grid_search(data,brute,{"finish":None})) # grid search order, grid search score
-    
+    results.append(brute_search(data)) 
     min_score = 100000000
     best_order = ()
     for result in results:
@@ -195,7 +196,6 @@ class Command(BaseCommand):
         labor_categories = [elem.labor_key for elem in LaborCategoryLookUp.objects.all()]
         order_terms = []
         for labor_category in labor_categories:
-            print("got here")
             labor_objects = LaborCategoryLookUp.objects.filter(labor_key=labor_category)
             df = pd.DataFrame()
             for labor_object in labor_objects:
@@ -205,6 +205,7 @@ class Command(BaseCommand):
             df = df.set_index("Date")
             df.sort_index(inplace=True)
             model, new_data, order = trend_predict(df)
+            
             order_terms.append(order)
             predicted_data = pd.DataFrame()
             for ind in range(len(model.fittedvalues)):
@@ -222,7 +223,6 @@ class Command(BaseCommand):
                     fitted_values_by_category = FittedValuesByCategory(labor_key=labor_category, fittedvalue=predicted_data["price"][ind], start_date=predicted_data.index[ind])
                     fitted_values_by_category.save()
                 except:
-                    
                     code.interact(local=locals())
         #code.interact(local=locals())
 # results = sm.tsa.ARIMA(dta, (3,1,0)).fit()
