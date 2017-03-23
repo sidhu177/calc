b11713a98c0254265d425ebaec2916f7c62ec16d
Atul Varma <varmaa@gmail.com>
2017-03-23 15:38:31 -0400

    Use f-strings now that we have new mypy.

--- styleguide/storybook.py
+++ styleguide/storybook.py
@@ -8,5 +8,5 @@ def get_url(host: str) -> str:
     if STORYBOOK_PORT:
         if ':' in host:
             host = host.split(':')[0]
-        return 'http://{}:{}/'.format(host, STORYBOOK_PORT)
+        return f'http://{host}:{STORYBOOK_PORT}/'
     return '/storybook/'
